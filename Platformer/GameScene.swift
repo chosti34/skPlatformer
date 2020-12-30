21555ccd6fd8a79dde8c767fc0f233d591d51511
Timur Karimov <chosti34@gmail.com>
2020-12-30 23:59:20 +0300

    * Камера теперь не выходит за границы карты * Рефакторинг GameScene.swift

--- Platformer/GameScene.swift
+++ Platformer/GameScene.swift
@@ -11,44 +11,32 @@ import GameplayKit
 class GameScene: SKScene {
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
-    var contactHandler = PhysicsContactHandler()
-    
-    var characterNode: CharacterNode?
-    
     private var lastUpdateTime : TimeInterval = 0
     
+    var characterNode: CharacterNode?
     var parallaxSystem: GKComponentSystem<ParallaxComponent>?
+    var physicsContactDelegate = PhysicsContactDelegate()
     
     override func sceneDidLoad() {
         self.lastUpdateTime = 0
     }
 
     override func didMove(to view: SKView) {
-        parallaxSystem = GKComponentSystem.init(componentClass: ParallaxComponent.self)
+        guard let player = childNode(withName: "Player") as? CharacterNode else { return }
+        characterNode = player
+        characterNode?.initialise()
         
-        for entity in entities {
-            parallaxSystem?.addComponent(foundIn: entity)
-        }
+        guard let camera = camera else { return }
+        setCameraTilemapConstraint(camera)
         
-        for component in parallaxSystem?.components ?? [] {
-            component.initialise(with: camera)
-        }
+        let playerComponent = characterNode?.entity?.component(ofType: PlayerComponent.self)
+        playerComponent?.setup(camera: camera, scene: self)
         
-        if let node = childNode(withName: "Player") as? CharacterNode, let camera = camera {
-            characterNode = node
-            
-            // Setup player component
-            let playerComponent = node.entity?.component(ofType: PlayerComponent.self)
-            playerComponent?.setup(camera: camera, scene: self)
-            
-            // Setup animation component
-            let animationComponent = node.entity?.component(ofType: AnimationComponent.self)
-            animationComponent?.setup()
-            
-            // Setup player node
-            characterNode?.initialise()
-        }
-        physicsWorld.contactDelegate = contactHandler
+        let animationComponent = characterNode?.entity?.component(ofType: AnimationComponent.self)
+        animationComponent?.setup()
+        
+        physicsWorld.contactDelegate = physicsContactDelegate
+        initialiseParallaxSystem()
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -79,5 +67,33 @@ class GameScene: SKScene {
     private func makeCameraFollowNode(node: SKNode) {
         guard let camera = camera else { return }
         camera.run(SKAction.move(to: CGPoint(x: node.position.x, y: node.position.y), duration: 0.5))
+    }
+    
+    // https://stackoverflow.com/questions/35137563/clamping-camera-around-the-background-of-a-scene-in-spritekit
+    private func setCameraTilemapConstraint(_ camera: SKCameraNode) {
+        guard let tilemap = childNode(withName: "Ground Tile Map") as? SKTileMapNode else { return }
+        let tilemapContentRect = tilemap.calculateAccumulatedFrame()
+        
+        let scaledSize = CGSize(width: size.width * camera.xScale, height: size.height * camera.yScale)
+        
+        let xInset = min(scaledSize.width / 2, tilemap.mapSize.width / 2)
+        let yInset = min(scaledSize.height / 2, tilemap.mapSize.height / 2)
+        let insetContentRect = tilemapContentRect.insetBy(dx: xInset, dy: yInset)
+        
+        let xRange = SKRange(lowerLimit: insetContentRect.minX, upperLimit: insetContentRect.maxX)
+        let yRange = SKRange(lowerLimit: insetContentRect.minY, upperLimit: insetContentRect.maxY)
+        
+        let edgeConstraint = SKConstraint.positionX(xRange, y: yRange)
+        camera.constraints = [edgeConstraint]
+    }
+    
+    private func initialiseParallaxSystem() {
+        parallaxSystem = GKComponentSystem.init(componentClass: ParallaxComponent.self)
+        for entity in entities {
+            parallaxSystem?.addComponent(foundIn: entity)
+        }
+        for component in (parallaxSystem?.components ?? []) {
+            component.initialise(with: camera)
+        }
     }
 }
--- Platformer/PhysicsContactDelegate.swift
+++ Platformer/PhysicsContactDelegate.swift
@@ -14,7 +14,7 @@ struct Collider {
     static let ground: UInt32 = 0x01 << 1
 }
 
-class PhysicsContactHandler: NSObject, SKPhysicsContactDelegate {
+class PhysicsContactDelegate: NSObject, SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
         let collision: UInt32 = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
         if collision == Collider.player | Collider.ground {
